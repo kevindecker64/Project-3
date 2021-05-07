9319f45c808a11354795a31375cf7f6c4769ad34
Kevin Decker <KevinDecker64@gmail.com>
2021-05-06 22:49:17 -0500

    fixes login to not let you login with wrong password

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -35,7 +35,10 @@ async function login(req, res) {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) throw new Error();
-    await bcrypt.compare(req.body.password, user.password);
+    const check = await bcrypt.compare(req.body.password, user.password);
+    if (check === false) {
+      return res.status(400).json("Bad Credentials")
+    }
     const token = createJWT(user);
     res.json(token);
   } catch {
